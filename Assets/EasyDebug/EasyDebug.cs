d75c79b12d8510d893ab58430346a07230a0eb90
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-11-14 17:58:03 +0300

    Add LogWarning & LogError functions

--- Assets/EasyDebug/EasyDebug.cs
+++ Assets/EasyDebug/EasyDebug.cs
@@ -6,4 +6,26 @@ public static class EasyDebug
     {
         Debug.Log(string.Join(" ", objects));
     }
+    public static void Log(string separator, params object[] objects)
+    {
+        Debug.Log(string.Join(separator, objects));
+    }
+
+    public static void LogWarning(params object[] objects)
+    {
+        Debug.LogWarning(string.Join(" ", objects));
+    }
+    public static void LogWarning(string separator, params object[] objects)
+    {
+        Debug.LogWarning(string.Join(separator, objects));
+    }
+
+    public static void LogError(params object[] objects)
+    {
+        Debug.LogError(string.Join(" ", objects));
+    }
+    public static void LogError(string separator, params object[] objects)
+    {
+        Debug.LogError(string.Join(separator, objects));
+    }
 }
--- Assets/EasyDebug/Example/MainExample.cs
+++ Assets/EasyDebug/Example/MainExample.cs
@@ -4,5 +4,6 @@ public class MainExample : MonoBehaviour
     private void Start()
     {
         EasyDebug.Log(1, 2, 3);
+        EasyDebug.LogWarning(1, 2, 3, "as", new int[4] { 1, 2, 4, 5 });
     }
 }
