e353874bdbd3ec9990f64029fea2b1657c6fac21
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-11-14 17:26:37 +0300

    Init

---
+++ Assets/EasyDebug/EasyDebug.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Linq;
+public static class EasyDebug
+{
+    public static void Log(params object[] objects)
+    {
+        Debug.Log(string.Join(" ", objects));
+    }
+}
---
+++ Assets/EasyDebug/Example/MainExample.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class MainExample : MonoBehaviour
+{
+    private void Start()
+    {
+        EasyDebug.Log(1, 2, 3);
+    }
+}
