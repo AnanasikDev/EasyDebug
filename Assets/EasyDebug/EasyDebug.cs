1bdcd2a58d6c052f04ba5a0cbbbd0c9aeab241ff
Олег Бровченков <oleg.brovchenkov@gmail.com>
2022-06-23 11:44:51 +0300

    Bug fixed

--- Assets/EasyDebug/EasyDebug.cs
+++ Assets/EasyDebug/EasyDebug.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
-using System.Linq;
 public static class EasyDebug
 {
     public static void Log(params object[] objects)
     {
         Debug.Log(string.Join(" ", objects));
     }
-    public static void Log(string separator, params object[] objects)
+    public static void LogSep(string separator, params object[] objects)
     {
         Debug.Log(string.Join(separator, objects));
     }
@@ -15,7 +14,7 @@ public static class EasyDebug
     {
         Debug.LogWarning(string.Join(" ", objects));
     }
-    public static void LogWarning(string separator, params object[] objects)
+    public static void LogWarningSep(string separator, params object[] objects)
     {
         Debug.LogWarning(string.Join(separator, objects));
     }
@@ -24,7 +23,7 @@ public static class EasyDebug
     {
         Debug.LogError(string.Join(" ", objects));
     }
-    public static void LogError(string separator, params object[] objects)
+    public static void LogErrorSep(string separator, params object[] objects)
     {
         Debug.LogError(string.Join(separator, objects));
     }
