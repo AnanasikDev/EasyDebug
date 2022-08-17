cd93d9354d5e6550dd08a0ae355bb4326f0d7ee6
Олег Бровченков <oleg.brovchenkov@gmail.com>
2022-08-17 16:47:24 +0300

    Syntax complete redesign

--- Assets/EasyDebug/EasyDebug.cs
+++ Assets/EasyDebug/EasyDebug.cs
@@ -1,43 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-public static class EasyDebug
+using System.Collections;
+using System.Linq;
+using System;
+
+namespace EasyDebug
 {
-    public static void Log(params object[] objects)
+    public class Entity
     {
-        Debug.Log(string.Join(" ", objects));
+        public string value;
+        private object[] objects;
+
+        public Entity(object[] _objects)
+        {
+            this.objects = _objects;
+        }
+        public Entity(string _value)
+        {
+            this.value = _value;
+        }
+
+        public Entity Parse(string separator = null)
+        {
+            value = EDebug.ParseFunction(objects, separator != null ? separator : EDebug.defaultSeparator);
+            return this;
+        }
+        public void Do()
+        {
+            UnityEngine.Debug.Log(value);
+        }
     }
-    public static void LogSep(string separator, params object[] objects)
+    public static class EDebug
     {
-        Debug.Log(string.Join(separator, objects));
-    }
-    public static void LogCollection<T>(IEnumerable<T> collection)
-    {
-        Debug.Log(string.Join(" ", collection));
+        public static bool serialize = true;
+        public static string defaultSeparator = " ";
+
+        /// <summary>
+        /// Input values ; separator ; output string
+        /// </summary>
+        public static Func<object[], string, string> ParseFunction = SoftParse;
+
+        public static string SoftParse(object[] objects, string separator = null)
+        {
+            return string.Join(separator != null ? separator : EDebug.defaultSeparator, objects);
+        }
+        public static string DeepParse(object[] objects, string separator = null)
+        {
+            return string.Join(separator != null ? separator : EDebug.defaultSeparator, objects.Select(x => x is IEnumerable i && !(x is string) ? string.Join(separator, i.CastToStrings()) : x.ToString()));
+        }
+
+        public static Entity Commit(params object[] values)
+        {
+            return new Entity(values);
+        }
     }
 
-    public static void LogWarning(params object[] objects)
-    {
-        Debug.LogWarning(string.Join(" ", objects));
-    }
-    public static void LogWarningSep(string separator, params object[] objects)
-    {
-        Debug.LogWarning(string.Join(separator, objects));
-    }
-    public static void LogWarningCollection<T>(IEnumerable<T> collection)
-    {
-        Debug.LogWarning(string.Join(" ", collection));
-    }
+}
 
-    public static void LogError(params object[] objects)
+public static class Extensions
+{
+    public static IEnumerable<string> CastToStrings(this IEnumerable ienumerable)
     {
-        Debug.LogError(string.Join(" ", objects));
-    }
-    public static void LogErrorSep(string separator, params object[] objects)
-    {
-        Debug.LogError(string.Join(separator, objects));
-    }
-    public static void LogErrorCollection<T>(IEnumerable<T> collection)
-    {
-        Debug.LogError(string.Join(" ", collection));
+        foreach (var obj in ienumerable)
+            yield return obj.ToString();
     }
 }
+
+/*
+
+Syntax
+ 
+Debug.Log("Hello").Parse().Tag(tags.Debug).Do();
+Debug.LogWarning("wow").Do();
+Debug.Dog();
+Debug.DogWarning("oowow");
+
+Debug.Commit("Hello").Parse().Tag(tags.Debug).Do();
+Debug.Dommit("eh");
+
+ */
--- Assets/EasyDebug/Example/MainExample.cs
+++ Assets/EasyDebug/Example/MainExample.cs
@@ -1,15 +1,13 @@
 ﻿using UnityEngine;
+using EasyDebug;
+
 public class MainExample : MonoBehaviour
 {
     private void Start()
     {
-        EasyDebug.Log(1, 2, 3);
-        EasyDebug.LogWarning(1, 2, 3, "as", new int[4] { 1, 2, 4, 5 });
-        EasyDebug.Log(new int[3] { 1, 2, 3 });
-        EasyDebug.LogCollection(new int[3] { 1, 2, 3 });
-        EasyDebug.LogErrorCollection(new int[4] { 0, -1, 2, 3 });
-
-        EasyDebug.Log("halo");
-        EasyDebug.LogWarningSep(", ", 1, 2, 3, 4, 5, 6, 7);
+        EDebug.ParseFunction = EDebug.DeepParse;
+        
+        EDebug.Commit("Hello", "World").Parse(" ").Do();
+        EDebug.Commit(new int[3] { 1, 2, 3 }, 1, "HAlo").Parse(" ").Do();
     }
 }
